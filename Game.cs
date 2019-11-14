1d2f9a011faa05de3aafbac6fa47ab30d042d847
mhane142 <mhane142@students.bju.edu>
2019-11-14 16:58:09 -0500

    Previous push's changes actually made.

---
+++ Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Ascend
+{
+    class Game
+    {
+        public List<Platform> Platforms { get; set; }
+
+        public Player CurrentPlayer = new Player();
+
+        Timer GameBeginTimer;
+
+        public Game()
+        {
+            // Set up start of game
+            // Once setup is finished,
+
+            // Make Timer start game once countdown is finished
+            // Once game is started, Timer moves all objects on-screen downwards until player dies, as well as increasing score over time
+        }
+
+        public void StartGame()
+        {
+            // Code to start a game sequence
+        }
+    }
+}
---
+++ Platform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ascend
+{
+    class Platform
+    {
+        public int[] Dimensions { get; set; } = new int[2];
+
+        public int[] Location { get; set; } = new int[2];
+
+        public void ScrollDown (int distance)
+        {
+            Location[1] -= distance;
+        }
+    }
+}
